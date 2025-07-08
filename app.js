c2d05f6bcf37f53e399e99e98e5812529a87f8ce
Israel Linares <isralinares2003@gmail.com>
2025-07-08 11:47:44 -0400

    add file .gitignore

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+});
+
